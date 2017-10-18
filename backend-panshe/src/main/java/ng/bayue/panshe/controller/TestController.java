717aef573190e68a7592153942f1dde2f3d01d02
FENGYTS\fengyts <fengyts@163.com>
2017-10-18 23:47:14 +0800

    new

--- backend-panshe/src/main/java/ng/bayue/panshe/controller/TestController.java
+++ backend-panshe/src/main/java/ng/bayue/panshe/controller/TestController.java
@@ -5,15 +5,17 @@ import ng.bayue.panshe.common.ResultMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "test")
+@RequestMapping(value = "/test/")
 public class TestController extends BaseController{
 
 	@RequestMapping(value = "list", method = {RequestMethod.POST})
+	@ResponseBody
 	public ResultMessage list(){
 		
-		return ResultMessage.success();
+		return ResultMessage.success("haha");
 	}
 	
 }
